80ec477dc581161d4ad8f8311c988446d5769f99
Dean Joubert <development@awesomelegend.co.za>
2022-02-02 23:08:46 +0200

    Finished Course Project section for the baics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROoFCvaFJ9YLX3X-FYI8_C82FLQ_HuSoAPEA&usqp=CAU')
+  ];
   constructor() { }
 
   ngOnInit(): void {
